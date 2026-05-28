5932539eeb1d666c3c30d672d871b10fdda0ac93
Artemiii990 <pelmenosadchuk@gmail.com>
2026-05-29 01:50:32 +0200

    Added Swagger JWT auth

--- Backend/DockerBackend/ProductService/Program.cs
+++ Backend/DockerBackend/ProductService/Program.cs
@@ -13,6 +13,7 @@ using Amazon.SecretsManager.Model;
 using Elastic.Clients.Elasticsearch;
 
 using System.Text.Json;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -71,7 +72,58 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "ProductService",
+            Version = "v1"
+        }
+    );
+
+    options.AddSecurityDefinition(
+        "Bearer",
+        new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+
+            Type = SecuritySchemeType.Http,
+
+            Scheme = "bearer",
+
+            BearerFormat = "JWT",
+
+            In = ParameterLocation.Header,
+
+            Description =
+                "Enter JWT token"
+        }
+    );
+
+    options.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference =
+                        new OpenApiReference
+                        {
+                            Type =
+                                ReferenceType
+                                    .SecurityScheme,
+
+                            Id = "Bearer"
+                        }
+                },
+
+                Array.Empty<string>()
+            }
+        }
+    );
+});
 
 // =====================================
 // DATABASE
